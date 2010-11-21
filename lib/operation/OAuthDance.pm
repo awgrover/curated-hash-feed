de284084180ef5094c448b091e1b0405c956c8c6
Alan Grover <awgrover@msen.com>
2010-11-20 21:44:19 -0500

    oauth dance! clean up

--- lib/operation/OAuthDance.pm
+++ lib/operation/OAuthDance.pm
@@ -7,7 +7,7 @@ use Verbose; use Data::Dumper;
 sub do {
   # Start the dance
   my $self=shift;
-  my ($id) = @_;
+  my ($id, $params) = @_;
   vverbose 0,"Called with ",Dumper(\@_);
   # FIXME: $session->{'twitters'}->search(id=>...)
   my $cf = Model::CuratedFeed->find({id=>$id});
@@ -20,24 +20,60 @@ sub do {
     my $rez = Twitter_Util::twitter_query(
       { 
         account => $cf->twitter_account,
-        consumer_key => $oauth_config->{'oauth_key'},
+        consumer_key => $oauth_config->{'consumer_key'},
         consumer_secret => $oauth_config->{'consumer_secret'},
       },
       POST => 'https://api.twitter.com/oauth/request_token', # NOT JSON
       oauthtype => 'request token',
-      callback => $self->webApp->request->url('-path' => 1),
+      callback => $self->webApp->request->url('-path' => 1)."?op=OAuthDance.requestToken&id=$id",
       );
 
     my $uri = URI->new;
     $uri->query($rez);
     my %request_token = $uri->query_form;
 
-    die "Got request token: ",Dumper(\%request_token)," ";
-                 
+    $cf->update({oauth_token_secret => $request_token{'oauth_token_secret'}});
+
+    $params->{'redirect'} .= '?oauth_token='.$request_token{'oauth_token'};
+    vverbose 0,"Set redir to ".$params->{'redirect'};
+    1;             
     }
   else {
-    $self->{'pageContext'}->{'error'} = "Don't know about the twitter feed";
+    $self->webApp->pageContext->{'error'} = "Don't know about the twitter feed";
     }
+  }
+
+sub do_get_requestToken {
+  my $self=shift;
+  my ($params) = @_;
+  
+  my $cf = Model::CuratedFeed->find({id => $params->{'id'}});
+  my $oauth_config = $self->webApp->config->{'oauth'};
+
+  my $rez = Twitter_Util::twitter_query(
+    {
+      account => $cf->twitter_account,
+      consumer_key => $oauth_config->{'consumer_key'},
+      consumer_secret => $oauth_config->{'consumer_secret'},
+      verifier => $params->{'oauth_verifier'},
+      token => $params->{'oauth_token'},
+      token_secret =>  $cf->oauth_token_secret,
+    },
+    POST => 'https://api.twitter.com/oauth/access_token', # NOT JSON
+    oauthtype => 'access token',
+    );
+
+  my $uri = URI->new;
+  $uri->query($rez);
+  my %request_token = $uri->query_form;
+
+  vverbose 0,"params ",Dumper($params);
+
+  $cf->update({
+    oauth_token => $params->{'oauth_token'},
+    });
+ 
+   $self->webApp->pageContext->{'info'} = "Authenticated ".$cf->{'twitter_account'};
   }
 
 package Twitter_Util;
